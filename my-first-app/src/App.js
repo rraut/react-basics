a994b4f560f8c02172720e9d9041c9d070893908
Renuka Raut <renuka@Renukas-MacBook-Pro.local>
2017-11-21 14:28:40 +0530

    Your Message

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -2,39 +2,119 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-/*class App extends Component {
-/!*  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React </h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}*!/
-}*/
-class Shoppingelements extends React.Component{
-   /* constructor(props) {
+export default class Display extends React.Component{
+    constructor(props) {
         super(props);
-    }*/
-    showmore : function () {
-            document.getElementsByClassName("hidden").addClass("visible");
+        this.state = {
+            input: '',
+            firstNumber: 0,
+            secondNumber: 0,
+            operator: '',
+            end:false,
+            inputexpression:''
+        }
+        this.clear = this.clear.bind(this);
+        this.pressDigit = this.pressDigit.bind(this);
+        this.pressOperator = this.pressOperator.bind(this);
+        this.evaluate = this.evaluate.bind(this);
+
     }
+
+    componentWillUpdate(nextProps, nextState){
+        if((this.state.operator === '+' || this.state.operator === '-'|| this.state.operator === '*'|| this.state.operator === '/')  && nextState.secondNumber !== 0 && this.state.operator !== '=') {
+           // this.setState({input: this.state.firstNumber + this.state.secondNumber, operator: ''});
+            switch (this.state.operator) {
+                case '+':
+                    this.setState({input: this.state.firstNumber + this.state.secondNumber,operator:'',firstNumber:''});
+                    break;
+                case '-':
+                    this.setState({input: this.state.firstNumber - this.state.secondNumber});
+                    break;
+                case '*':
+                    this.setState({input:this.state.firstNumber * this.state.secondNumber,operator:''});
+                    break;
+                case '/':
+                    this.setState({input:this.state.firstNumber / this.state.secondNumber,operator:''});
+                    break;
+                case '=':
+                    this.setState({inputexpression:this.state.input,operator:'',firstNumber:'',secondNumber:''});
+                    break;
+                default:
+                    console.log('Sorry, we are out of ' + this.state.operator + '.');
+            }
+        }
+            else if(this.state.operator === '='){
+                this.setState({operator:'',firstNumber:'',secondNumber:''});
+            }
+
+            console.log("Is there anything else you'd like?");
+        }
+
+
+    clear = function () {
+        this.setState({
+            input:'',
+            firstNumber:0,
+            secondNumber:0,
+            operator:''
+
+        });
+    };
+
+    pressDigit = function (val) {
+        if(this.state.firstNumber!==0){
+            this.setState({
+                secondNumber:val
+            });
+        }
+        if(this.state.firstNumber==0){
+            this.setState({
+                input:val,
+                firstNumber:val
+            });
+        }
+
+    }
+
+    pressOperator = function (currentOperator) {
+        this.setState({firstNumber: parseInt(this.state.input), input: '', operator: currentOperator});
+    }
+
+    evaluate = function () {
+        this.setState({
+            secondNumber: parseInt(this.state.input)
+        })
+    }
+
     render(){
         return(
-            <div className="shoppingList">
-                <ul>
-                    <li onClick={showmore}>{this.props.name}</li>
-                    <li className='hidden'>{this.props.price}</li>
-                    <li className='hidden'>{this.props.quantity}</li>
-                </ul>
+            <div className="calculator">
+                <div className="inputExpression" >
+                    <div>{this.state.firstNumber}{this.state.operator}{this.state.secondNumber}</div>
+                </div>
+                <div className="keyholder">
+                    <div className="calculator-row">
+                        <button className="button" onClick={() => this.pressDigit(1)}>1</button>
+                        <button className="button" onClick={() => this.pressDigit(2)}>2</button>
+                        <button className="button" onClick={() => this.pressDigit(3)}>3</button>
+                        <button className="button" onClick={() => this.pressDigit(4)}>4</button>
+                        <button className="button" onClick={() => this.pressDigit(5)}>5</button>
+                        <button className="button" onClick={() => this.pressDigit(6)}>6</button>
+                        <button className="button" onClick={() => this.pressDigit(7)}>7</button>
+                        <button className="button" onClick={() => this.pressDigit(8)}>8</button>
+                        <button className="button" onClick={() => this.pressDigit(7)}>9</button>
+                        <button className="button" onClick={() => this.pressDigit(0)}>0</button>
+                        <button className="button" onClick={() => this.pressOperator('+')}> + </button>
+                        <button className="button" onClick={() => this.pressOperator('-')}> - </button>
+                        <button className="button" onClick={() => this.pressOperator('/')}> / </button>
+                        <button className="button" onClick={() => this.pressOperator('*')}> * </button>
+                        <button className="button" onClick={() => this.evaluate() }> = </button>
+                        <button className="button" onClick={() => this.clear()}> clear </button>
+
+                    </div>
+                </div>
             </div>
         );
     }
+
 }
-export default Shoppingelements;
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Shoppingelements from './App'
+import Display from './App'
 import registerServiceWorker from './registerServiceWorker';
 const element = <h1>Hello, world</h1>;
-//ReactDOM.render(<span>Renuka</span>, document.getElementById('root'));
 
-export default Shoppingelements;
 ReactDOM.render(
     <div>
-        <Shoppingelements name='Banana' price='forty' quantity='12'/>
-        <Shoppingelements name='Apple' price='hundread' quantity='2'/>
+        <Display />
     </div>,document.getElementById("root"));
 
 registerServiceWorker();
